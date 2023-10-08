13a94a656dba7dcbd67a3b9579dc7ab0afd2f8f5
Jorge Jara <jorgejarainostroza@gmail.com>
2023-10-08 13:12:34 -0300

    Set up routes

--- app.py
+++ app.py
@@ -1,58 +1,27 @@
-import hashlib
-import datetime
-from flask import Flask, request, jsonify
-from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
-from pymongo import MongoClient
-from pymongo.server_api import ServerApi
-from bson.objectid import ObjectId
+#!/usr/bin/env python3
+
+from flask import Flask
+from flask_jwt_extended import JWTManager
 from dotenv import load_dotenv
 import os
+
+from database import Database
 
 load_dotenv()
 
 app = Flask(__name__)
 jwt = JWTManager(app)
 
-uri = os.getenv("MONGODB_URI")
+app.config["MONGO_URI"] = os.getenv("MONGODB_URI")
+app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
 
-client = MongoClient(uri, server_api=ServerApi(version='1'))
+Database().setup(app)
 
-db = client['udec']
-users = db['users']
+@app.route('/')
+def index():
+    return "Nada que ver aquí. Circulando, por favor"
 
-@app.route('/api/v1/users', methods=['POST'])
-@jwt_required()
-def create_user():  
-    new_user = request.get_json()
-    new_user['password'] = hashlib.sha256(new_user['password'].encode('utf-8')).hexdigest()
-    doc = users.find_one({
-        "username": new_user['username']
-    })
-
-    if doc:
-        return jsonify({
-            "success": False,
-            "message": "Username already exists"
-        }), 400
-    
-    users.insert_one(new_user)
-    return jsonify({
-        "success": True,
-        "message": "User created successfully"
-    }), 201
-
-@app.route('/api/v1/login', methods=['POST'])
-def login():  
-    login_details = request.get_json()
-    user = users.find_one({"username": login_details['username']})
-    
-    if user:
-        enc_pass = hashlib.sha256(login_details['password'].encode('utf-8')).hexdigest()
-        if enc_pass == user['password']:
-            access_token = create_access_token(identity=str(user['username']))
-            return jsonify(access_token=access_token), 200
-        
-    return jsonify({"success": False, "message": "Wrong credentials"}), 401
+import routes.users # nopep8
 
 
 if __name__ == '__main__':
---
+++ routes/users.py
@@ -0,0 +1,45 @@
+from __main__ import app
+
+from flask import request, jsonify
+from flask_jwt_extended import create_access_token
+import hashlib
+
+from database import Database
+
+users = Database().pymongo.db.users
+
+@app.route('/api/v1/users', methods=['POST'])
+def create_user():  
+    new_user = request.get_json()
+    new_user['password'] = hashlib.sha256(new_user['password'].encode('utf-8')).hexdigest()
+    doc = users.find_one({
+        "username": new_user['username']
+    })
+
+    if doc:
+        return jsonify({
+            "success": False,
+            "message": "Username already exists"
+        }), 400
+    
+    users.insert_one(new_user)
+    return jsonify({
+        "success": True,
+        "message": "User created successfully"
+    }), 201
+
+@app.route('/api/v1/login', methods=['POST'])
+def login():  
+    login_details = request.get_json()
+    user = users.find_one({"username": login_details['username']})
+    
+    if user:
+        enc_pass = hashlib.sha256(login_details['password'].encode('utf-8')).hexdigest()
+        if enc_pass == user['password']:
+            access_token = create_access_token(identity=str(user['username']))
+            return jsonify({
+                "success": True,
+                "access_token": access_token
+            }), 200
+        
+    return jsonify({"success": False, "message": "Wrong credentials"}), 401
